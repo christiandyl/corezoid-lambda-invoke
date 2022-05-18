62784e4bc8b9bad4d7d1c288363e01adf4a60330
Christian Dyl <christian.dyl@outlook.com>
2022-05-18 16:45:31 +0300

    lambda invoke logic

--- usercode.js
+++ usercode.js
@@ -1,5 +1,28 @@
 var AWS = require('aws-sdk');
 
-module.exports = (data) => {
-  return { ...data, test: 'test' };
+module.exports = async ({
+  region = 'eu-west-1',
+  accessKeyId,
+  secretAccessKey,
+  functionName,
+  payload,
+}) => {
+  const lambda = new AWS.Lambda({
+    region,
+    credentials: { accessKeyId, secretAccessKey },
+  });
+
+  const params = {
+    FunctionName: functionName,
+    // ClientContext: 'STRING_VALUE',
+    InvocationType: 'RequestResponse',
+    LogType: 'None',
+    Payload: JSON.stringify(payload || {}),
+    // Qualifier: 'STRING_VALUE'
+  };
+
+  const lambdaResult = await lambda.invoke(lambdaParams).promise();
+  const result = JSON.parse(lambdaResult.Payload);
+
+  return result;
 };
